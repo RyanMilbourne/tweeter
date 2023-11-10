0a4896c0ff77653b3d2380e86aa3c9a07eb8ae73
Ryan Milbourne <ryanmmilbourne@gmail.com>
2023-11-09 16:35:19 -0800

    add character counter function to update remaining characters left in textarea

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,3 +1,22 @@
 $(document).ready(function() {
-  console.log("Testing successful link")
+
+  $('#tweet-text').on("input", function(event) {
+    const inputLength = this.value.length;
+    const startCount = 140;
+
+    const remainingChar = (startCount - inputLength);
+
+    const ancestor = $(this).closest('.create-tweet');
+
+    const counter = ancestor.find('.counter');
+
+    counter.text(remainingChar);
+
+    if (remainingChar < 0) {
+      counter.addClass('exceeded-character-limit');
+    } else {
+      counter.removeClass('exceeded-character-limit');
+    }
+  });
+
 });
