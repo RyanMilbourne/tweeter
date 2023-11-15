dbebdab657e55a21e7e637219ce0f16c5990fdc6
Ryan Milbourne <ryanmmilbourne@gmail.com>
2023-11-15 09:55:50 -0800

    remove /tweets post for compatibility with new js

--- server/index.js
+++ server/index.js
@@ -32,8 +32,6 @@ const tweetsRoutes = require("./routes/tweets")(DataHelpers);
 // Mount the tweets routes at the "/tweets" path prefix:
 app.use("/tweets", tweetsRoutes);
 
-app.post("/tweets")
-
 app.listen(PORT, () => {
   console.log("Example app listening on port " + PORT);
 });
